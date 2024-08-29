9ca9e3bf1fbbe2e6e4d6e05b2983dd956224cb46
nikhilreddy-d <doddanikhil2001@gmail.com>
2024-08-29 20:47:08 +0530

    added internet gateway and route to internet gateway

--- entire-aws/network.tf
+++ entire-aws/network.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "ntier_vpc" {
+resource "aws_vpc" "entier_vpc" {
   cidr_block = var.vpc_network_cidr
   tags = {
     Name = local.name
@@ -6,12 +6,42 @@ resource "aws_vpc" "ntier_vpc" {
 }
 resource "aws_subnet" "subnets" {
   count      = length(var.subnet_names)
-  vpc_id     = aws_vpc.ntier_vpc.id
+  vpc_id     = aws_vpc.entier_vpc.id
   cidr_block = cidrsubnet(var.vpc_network_cidr, 8, count.index)
   tags = {
     Name = var.subnet_names[count.index]
   }
   depends_on = [
-    aws_vpc.ntier_vpc
+    aws_vpc.entier_vpc
+  ]
+}
+data "aws_route_table" "default" {
+  vpc_id = aws_vpc.entier_vpc.id
+
+  depends_on = [
+    aws_vpc.entier_vpc
+  ]
+
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.entier_vpc.id
+
+  tags = {
+    Name = "entier igw"
+  }
+  depends_on = [
+    aws_vpc.entier_vpc
+  ]
+}
+
+resource "aws_route" "igwroute" {
+  route_table_id         = data.aws_route_table.default.id
+  destination_cidr_block = local.anywhere
+  gateway_id             = aws_internet_gateway.igw.id
+
+  depends_on = [
+    aws_vpc.entier_vpc,
+    aws_internet_gateway.igw
   ]
 }
